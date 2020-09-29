46d2f6fca4fbf11e81a681c5341a013a76bba9ff
krystiangorecki <10375973+krystiangorecki@users.noreply.github.com>
2020-09-29 20:14:21 +0200

    Update steam-to-bazar-link.js

--- steam-to-bazar-link.js
+++ steam-to-bazar-link.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         Steam to Bazar link
 // @author       krystiangorecki
-// @version      2020.09.16
+// @version      2020.09.31
 // @namespace    https://github.com/krystiangorecki/steamgifts-userscripts/
 // @icon         https://upload.wikimedia.org/wikipedia/commons/8/83/Steam_icon_logo.svg
 // @match        https://store.steampowered.com/app/*
 // @updateURL    https://raw.githubusercontent.com/krystiangorecki/userscripts/master/steam-to-bazar-link.js
+// @downloadURL  https://raw.githubusercontent.com/krystiangorecki/userscripts/master/steam-to-bazar-link.js
 // @grant        none
 // ==/UserScript==
 
@@ -18,7 +19,7 @@ function execute(){
     var header = document.querySelector('div.apphub_HomeHeaderContent div.apphub_AppName');
     debugger;
     if(header!=undefined) {
-        var gameTitle = header.innerText.replaceAll('®','').replaceAll('™','').replaceAll(':','');
+        var gameTitle = header.innerText.replaceAll('®','').replaceAll('™','').replaceAll(':','').replace('-',' ');
         var bazarLink = document.createElement("a");
         bazarLink.innerHTML = ' <img src="https://bazar.lowcygier.pl/favicon.ico" style="height:20px"/>'
         bazarLink.classList.add('bazar');
