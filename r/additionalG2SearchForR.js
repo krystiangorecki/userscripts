5e39bd2d5653dff9091dfaa90251246820097d1e
krystiangorecki <10375973+krystiangorecki@users.noreply.github.com>
2021-10-08 14:12:10 +0200

    Update additionalG2SearchForR.js

--- r/additionalG2SearchForR.js
+++ r/additionalG2SearchForR.js
@@ -12,7 +12,7 @@ var g = atob('Z2Fyc29uaWVyYS5jb20ucGw=');
 
 (function() {
     'use strict';
-    phoneSearchG2();
+    phoneSearchG();
     boxSearchByAdNumber();
 
 })();
@@ -28,13 +28,13 @@ function boxSearchByAdNumber() {
     });
 }
 
-function phoneSearchG2() {
+function phoneSearchG() {
     var phoneInput = document.querySelector('#anons_tel');
     if (phoneInput != undefined && phoneInput.value.length == 9) {
         var phoneNumberWithDashes = phoneInput.value.slice(0,3) + '-' + phoneInput.value.slice(3,6) + '-' + phoneInput.value.slice(6,9) ;
         var url = 'http://' + g + '/forum/index.php?app=googlecse#gsc.tab=0&gsc.q=%22' + phoneNumberWithDashes + '%22';
         var newLink = document.createElement("span");
-        newLink.innerHTML = ' <a href="' + url + '" style="   text-decoration: none;        text-align: center;    display: inline-block;    border-width: 2px;    border-style: solid;    border-color: red;   padding: 1px 2px;    " > g2 </a>';
+        newLink.innerHTML = ' <a href="' + url + '" style="   text-decoration: none;        text-align: center;    display: inline-block;    border-width: 2px;    border-style: solid;    border-color: red;   padding: 1px 2px;    " > g </a>';
         insertAfter(phoneInput, newLink);
     }
 }
