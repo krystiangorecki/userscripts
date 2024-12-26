c469355b1ad40bf14e4fac9e60f3e5a9406b0930
krystiangorecki <10375973+krystiangorecki@users.noreply.github.com>
2024-12-26 22:22:47 +0100

    Update Youtube speed control.js

--- Youtube speed control.js
+++ Youtube speed control.js
@@ -1,29 +1,51 @@
 // ==UserScript==
 // @name         Youtube speed control
 // @namespace    https://github.com/krystiangorecki/userscripts/
-// @version      1.1
+// @version      1.4
 // @description  "Direction is more important than speed. You can go fast in the wrong direction."
 // @match        https://www.youtube.com/watch*
+// @match        https://www.youtube.com/live/*
 // @icon         https://www.youtube.com/s/desktop/4fc9328c/img/favicon.ico
 // @updateURL    https://raw.githubusercontent.com/krystiangorecki/userscripts/master/Youtube%20speed%20control.js
 // @downloadURL  https://raw.githubusercontent.com/krystiangorecki/userscripts/master/Youtube%20speed%20control.js
 // @grant        none
 // ==/UserScript==
 
+var retry = 0;
+var max_retry = 20;
+
 (function() {
     'use strict';
-    setTimeout(function() { addManualStartButton(1);   }, 200);
-    setTimeout(function() { addManualStartButton(1.25);}, 200);
-    setTimeout(function() { addManualStartButton(1.5); }, 200);
-    setTimeout(function() { addManualStartButton(1.75);}, 200);
-    setTimeout(function() { addManualStartButton(2);   }, 200);
-    setTimeout(function() { addManualStartButton(2.1); }, 200);
-    setTimeout(function() { addManualStartButton(2.2); }, 200);
-    setTimeout(function() { addManualStartButton(2.3); }, 200);
-    setTimeout(function() { addManualStartButton(2.4); }, 200);
-    setTimeout(function() { addManualStartButton(2.5); }, 200);
-    setTimeout(function() { addManualStartButton(3);   }, 200);
+    exec();
 })();
+
+function exec() {
+    var buttons = document.querySelector('#buttons');
+    if (buttons==undefined) {
+        if (retry < max_retry) {
+            retry++;
+            console.log('adding speed control - retry ' + retry);
+            setTimeout(exec, 500);
+        } else {
+            console.log('adding speed control - retry limit reached //' + max_retry);
+        }
+    } else {
+        setTimeout(function() { addManualStartButton(1);   }, 200);
+        setTimeout(function() { addManualStartButton(1.25);}, 200);
+        setTimeout(function() { addManualStartButton(1.5); }, 200);
+        setTimeout(function() { addManualStartButton(1.75);}, 200);
+        setTimeout(function() { addManualStartButton(2);   }, 200);
+        setTimeout(function() { addManualStartButton(2.1); }, 200);
+        setTimeout(function() { addManualStartButton(2.2); }, 200);
+        setTimeout(function() { addManualStartButton(2.3); }, 200);
+        setTimeout(function() { addManualStartButton(2.4); }, 200);
+        setTimeout(function() { addManualStartButton(2.5); }, 200);
+        setTimeout(function() { addManualStartButton(2.6); }, 200);
+        setTimeout(function() { addManualStartButton(2.7); }, 200);
+        setTimeout(function() { addManualStartButton(2.8); }, 200);
+        setTimeout(function() { addManualStartButton(3);   }, 200);
+    }
+}
 
 function insertAfter(referenceNode, newNode) {
     referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
@@ -37,13 +59,15 @@ function insertAsFirstChild(referenceNode, newNode) {
 
 function addManualStartButton(speed) {
     var newLink = document.createElement("a");
-    newLink.innerHTML = ' &nbsp;&nbsp;' + speed + '&nbsp;&nbsp; ';
+    newLink.textContent = speed;
     newLink.id = 'startButton'+speed;
-    newLink.style = 'color:#696969;';
-    insertBefore(document.querySelector('#buttons'), newLink);
+    newLink.style = 'color:#696969; margin-right:5px';
+    var buttons = document.querySelector('#buttons');
+    insertBefore(buttons, newLink);
     newLink.addEventListener("click", function(){setSpeed(speed)});
 }
 
 function setSpeed(speed) {
+    console.log('document.getElementsByTagName("video")[0].playbackRate = ' + speed);
     document.getElementsByTagName("video")[0].playbackRate = speed;
 }
