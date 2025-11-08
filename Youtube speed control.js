d145774a6c8aaa5b830cec7f0de5e67e8a150b6d
krystiangorecki <10375973+krystiangorecki@users.noreply.github.com>
2025-11-08 21:41:08 +0100

    Update Youtube speed control.js

--- Youtube speed control.js
+++ Youtube speed control.js
@@ -1,15 +1,19 @@
 // ==UserScript==
 // @name         Youtube speed control
 // @namespace    https://github.com/krystiangorecki/userscripts/
-// @version      1.4
+// @version      1.6
 // @description  "Direction is more important than speed. You can go fast in the wrong direction."
 // @match        https://www.youtube.com/watch*
 // @match        https://www.youtube.com/live/*
+// @match        https://www.youtube.com/shorts/*
 // @icon         https://www.youtube.com/s/desktop/4fc9328c/img/favicon.ico
 // @updateURL    https://raw.githubusercontent.com/krystiangorecki/userscripts/master/Youtube%20speed%20control.js
 // @downloadURL  https://raw.githubusercontent.com/krystiangorecki/userscripts/master/Youtube%20speed%20control.js
 // @grant        none
 // ==/UserScript==
+
+// v1.5 works for shorts
+// v1.6 custom labels and fast forward
 
 var retry = 0;
 var max_retry = 20;
@@ -20,6 +24,7 @@ var max_retry = 20;
 })();
 
 function exec() {
+    debugger;
     var buttons = document.querySelector('#buttons');
     if (buttons==undefined) {
         if (retry < max_retry) {
@@ -31,6 +36,8 @@ function exec() {
         }
     } else {
         setTimeout(function() { addManualStartButton(1);   }, 200);
+        setTimeout(function() { addManualStartButton(1.05);}, 200);
+        setTimeout(function() { addManualStartButton(1.1); }, 200);
         setTimeout(function() { addManualStartButton(1.25);}, 200);
         setTimeout(function() { addManualStartButton(1.5); }, 200);
         setTimeout(function() { addManualStartButton(1.75);}, 200);
@@ -44,6 +51,7 @@ function exec() {
         setTimeout(function() { addManualStartButton(2.7); }, 200);
         setTimeout(function() { addManualStartButton(2.8); }, 200);
         setTimeout(function() { addManualStartButton(3);   }, 200);
+        setTimeout(function() { addManualStartButton(16, ">>"); }, 200);
     }
 }
 
@@ -57,9 +65,9 @@ function insertAsFirstChild(referenceNode, newNode) {
     referenceNode.insertBefore(newNode, referenceNode.firstChild);
 }
 
-function addManualStartButton(speed) {
+function addManualStartButton(speed, optionalLabel) {
     var newLink = document.createElement("a");
-    newLink.textContent = speed;
+    newLink.textContent = optionalLabel==undefined ? speed : optionalLabel;
     newLink.id = 'startButton'+speed;
     newLink.style = 'color:#696969; margin-right:5px';
     var buttons = document.querySelector('#buttons');
@@ -68,6 +76,17 @@ function addManualStartButton(speed) {
 }
 
 function setSpeed(speed) {
-    console.log('document.getElementsByTagName("video")[0].playbackRate = ' + speed);
-    document.getElementsByTagName("video")[0].playbackRate = speed;
+    let videoIndex;
+    if (window.location.pathname.startsWith("/shorts/")) {
+        const videos = document.getElementsByTagName("video");
+        for (const video of videos) {
+            if (!video.paused && !video.ended) {
+                console.log('video.playbackRate = ' + speed);
+                video.playbackRate = speed;
+            }
+        }
+    } else {
+        console.log('document.getElementsByTagName("video")[0].playbackRate = ' + speed);
+        document.getElementsByTagName("video")[0].playbackRate = speed;
+    }
 }
