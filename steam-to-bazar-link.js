25babf9f9af9795b18957660dbfb67eb652e751e
krystiangorecki <10375973+krystiangorecki@users.noreply.github.com>
2025-12-13 13:32:10 +0100

    Update version and refactor link addition logic

--- steam-to-bazar-link.js
+++ steam-to-bazar-link.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Steam to Bazar link (+Steamgifts +gg.deals)
 // @author       krystiangorecki
-// @version      2022.01.01
+// @version      2025.12.13
 // @namespace    https://github.com/krystiangorecki/userscripts/
 // @icon         https://upload.wikimedia.org/wikipedia/commons/8/83/Steam_icon_logo.svg
 // @match        https://store.steampowered.com/app/*
@@ -10,42 +10,50 @@
 // @grant        none
 // ==/UserScript==
 
-var header = document.querySelector('div.apphub_HomeHeaderContent div.apphub_AppName');
+var appName = document.querySelector('div.apphub_HomeHeaderContent div.apphub_AppName');
 
 (function() {
     'use strict';
-    setTimeout(addSteamgiftsLink, 100);
-    setTimeout(addBazarLink, 150);
-    setTimeout(addGGDealsLink, 200);
+    if (appName != undefined) {
+        setTimeout(setTitleDisplayInline, 50);
+        setTimeout(addGGDealsLink, 100);
+        setTimeout(addBazarLink, 110);
+        setTimeout(addSteamgiftsLink, 120);
+    }
 })();
 
+function setTitleDisplayInline(){
+    appName.style = appName.style + '; display:inline';
+}
+
 function addGGDealsLink(){
-    if(header!=undefined) {
-        var gameTitle = header.innerText.replaceAll('®','').replaceAll('™','').replaceAll(':','').replaceAll('-',' ').replaceAll('—',' ').replaceAll('’',' ').replaceAll('\'',' ').replaceAll('  ',' ').replaceAll('  ',' ').trim().replaceAll(' ','-').toLowerCase();
-        var ggdealsLink = document.createElement("a");
-        ggdealsLink.innerHTML = '<img src="https://gg.deals/favicon-48x48.png" style="height:20px; margin-left:10px;"/>'
-        ggdealsLink.setAttribute("href", "https://gg.deals/game/" + gameTitle);
-        header.appendChild(ggdealsLink);
-    }
+    var gameTitle = appName.innerText.replaceAll('®','').replaceAll('™','').replaceAll(':','').replaceAll('-',' ').replaceAll('—',' ').replaceAll('’','').replaceAll('\'','').replaceAll('.',' ').replaceAll('  ',' ').replaceAll('  ',' ').replaceAll('+','').trim().replaceAll(' ','-').replaceAll('--','-').toLowerCase();
+    var ggdealsLink = document.createElement("a");
+    ggdealsLink.innerHTML = '<img src="https://gg.deals/favicon-48x48.png" style="height:20px; margin-left:10px; user-select: none;"/>'
+    ggdealsLink.setAttribute("href", "https://gg.deals/game/" + gameTitle);
+    insertAfter(appName, ggdealsLink);
 }
 
 function addBazarLink(){
-    if(header!=undefined) {
-        var gameTitle = header.innerText.replaceAll('®','').replaceAll('™','').replaceAll(':','').replaceAll('-',' ').replaceAll('—',' ').replaceAll('’','\'');
-        var bazarLink = document.createElement("a");
-        bazarLink.innerHTML = '<img src="https://bazar.lowcygier.pl/favicon.ico" style="height:20px; margin-left:10px;"/>'
-        bazarLink.setAttribute("href", "https://bazar.lowcygier.pl/?options=&type=&platform=&payment=&game_type=&game_genre=&title=" + gameTitle + "&sort=-created_at&per-page=25");
-        header.appendChild(bazarLink);
-    }
+    var gameTitle = appName.innerText.replaceAll('®','').replaceAll('™','').replaceAll(':','').replaceAll('-',' ').replaceAll('—',' ').replaceAll('’','\'');
+    var bazarLink = document.createElement("a");
+    bazarLink.innerHTML = '<img src="https://bazar.lowcygier.pl/favicon.ico" style="height:20px; margin-left:10px; user-select: none;"/>'
+    bazarLink.setAttribute("href", "https://bazar.lowcygier.pl/?options=&type=&platform=&payment=&game_type=&game_genre=&title=" + gameTitle + "&sort=-created_at&per-page=25");
+    insertAfter(appName, bazarLink);
 }
 
 function addSteamgiftsLink(){
     var appIdElement = document.querySelector('div.glance_tags');
-    if(appIdElement!=undefined) {
+    if (appIdElement != undefined) {
         var appId = appIdElement.dataset.appid;
         var sgLink = document.createElement("a");
-        sgLink.innerHTML = ' <img src="https://cdn.steamgifts.com/img/favicon.ico" style="height:20px; margin-left:10px;"/>'
+        sgLink.innerHTML = ' <img src="https://cdn.steamgifts.com/img/favicon.ico" style="height:20px; margin-left:10px; user-select: none;"/>'
         sgLink.setAttribute("href", "https://www.steamgifts.com/giveaways/search?app=" + appId);
-        header.appendChild(sgLink);
+        insertAfter(appName, sgLink);
     }
+}
+
+
+function insertAfter(referenceNode, newNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
